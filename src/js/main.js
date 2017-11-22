81e5e4485a78c11b80b53605439414afffbf7cde
bvillain92@gmail.com <BVillain92@gmail.com>
2017-11-22 15:16:21 +0100

    svg generation

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import Router from './Classes/Router'
 import Landing from './Classes/Landing'
 import Header from './Classes/Header'
 import Creation from './Classes/Creation'
-import Rellax from 'rellax'
+import Visualization from './Classes/Visualization'
 
 window.addEventListener('touchstart', function () {
   window.IS_TOUCHSCREEN = true
@@ -10,6 +10,9 @@ window.addEventListener('touchstart', function () {
 
 new Router()
 new Header(document.querySelector('.header'))
-new Rellax('.rellax')
 new Creation()
 new Landing()
+
+const viz = new Visualization(document.querySelector('.performance__visualization-svg'), 6, ['blabla', 'bloblo', 'blublues', 'blibluer', 'bluro', 'bloeuri'], [0.85, 0.75, 0.65, 0.7, 0.85, 0.6])
+
+console.log(viz)
