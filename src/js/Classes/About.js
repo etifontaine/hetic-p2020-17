e2d464d75a06173e75c56375ad2bd07442e51577
bvillain92@gmail.com <BVillain92@gmail.com>
2017-11-23 12:30:15 +0100

    update about

--- src/js/Classes/About.js
+++ src/js/Classes/About.js
@@ -2,7 +2,7 @@ class About {
   constructor (container) {
     this.container = container
     this.video_play = container.querySelector('.about__button')
-    
+
     this.video_skip_container = container.querySelector('.video__skip-container')
     this.video_skip_btn = container.querySelector('.video__skip-button')
     this.video_skip_line = container.querySelector('.video__skip-line')
