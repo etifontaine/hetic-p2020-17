6230bc2a0c662afd635f79fc7363d5c14991ec26
Etienne FONTAINE <etienne.fontaine1@gmail.com>
2017-11-23 17:31:18 +0100

    update pwa

--- src/js/main.js
+++ src/js/main.js
@@ -18,7 +18,7 @@ new Landing()
 new Visualization(document.querySelector('.performance__visualization-svg'), 6, ['blabla', 'bloblo', 'blublues', 'blibluer', 'bluro', 'bloeuri'], [0.9, 0.75, 0.65, 0.7, 0.85, 0.6])
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js', {scope: '/'})
+  navigator.serviceWorker.register('./service-worker.js', {scope: './'})
     .then(() => console.log('Service Worker registered successfully.'))
     .catch(error => console.log('Service Worker registration failed:', error))
 }
