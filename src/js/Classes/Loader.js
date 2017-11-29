49f0963d909220adcec2d57f58eeb5079578cf8d
lucdandrel <luc.dandrel@hetic.net>
2017-11-29 18:43:51 +0100

    :lipstick: update loader

--- src/js/Classes/Loader.js
+++ src/js/Classes/Loader.js
@@ -2,13 +2,11 @@ import axios from 'axios'
 
 class Loader {
     constructor (container) {
-        this.srcElements = [].slice.call(
+        this.elementsToLoad = [].slice.call(
             document.querySelectorAll('[data-preload]')
         )
         this.loader = document.querySelector('.loader-2')
-        this.eventsList = ['progress', 'complete']
-        this.progressEvents = []
-        this.completeEvents = []
+        this.loaderPourcentage = document.querySelector('.loader-pourcentage')
         this.globalProgress = 0
         this.requests = []
 
@@ -17,49 +15,43 @@ class Loader {
     
     }
     init () {
-          this.srcElements.forEach(element => {
-      this.requests.push({
-        element,
-        type: 'src',
-        progress: 0
+      this.elementsToLoad.forEach(element => {
+        this.requests.push({
+          element,
+          type: 'src',
+          progress: 0
+        })
       })
-    })
 
-    this.requests.forEach((request, index) => {
-      axios.get(request.element.getAttribute(request.type), {
-        onDownloadProgress: e => {
-          const progress = Math.floor(e.loaded / e.total * 100)
-          
-          this.updateRequestProgress(index, progress)
-        }
+      this.requests.forEach((request, index) => {
+        axios.get(request.element.getAttribute(request.type), {
+          onDownloadProgress: e => {
+            const progress = Math.floor(e.loaded / e.total * 100)
+            
+            this.updateLoader(index, progress)
+          }
+        })
       })
-    })
-  
     }
-    /**
-   * updateRequestProgress()
-   * @param {number} index - Index of the request in the requests array
-   * @param {number} progress - Percentage of completion of the request
-   */
-  updateRequestProgress(index, progress) {
-    this.requests[index].progress = progress;
-    const total = this.requests.reduce(
-      (value, request) => value + request.progress,
-      0
-    );
+   
+    updateLoader(index, progress) {
+      this.requests[index].progress = progress;
+      const total = this.requests.reduce(
+        (value, request) => value + request.progress,
+        0
+      );
 
-    this.globalProgress = Math.floor(total / this.requests.length);
+      this.globalProgress = Math.floor(total / this.requests.length);
 
-    this.loader.style.clipPath = `inset(${100 - this.globalProgress}% 0 0 0)`
-    
-    if (this.globalProgress === 100) {
-        this.loader
-      } else {
-        console.log('encours')
-      }
-  }
+      this.loader.style.clipPath = `inset(${100 - this.globalProgress}% 0 0 0)`
+      this.loaderPourcentage.innerHTML = this.globalProgress + '%'
+      
+      if (this.globalProgress === 100) {
+          this.loader.parentElement.parentElement.style.transform = 'translateY(-100vh)'
+        } 
+    }
     
   }
-  
+
   export default Loader
   
